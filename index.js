5c2cf73af68a58aba3158d3f7280c3eaf12f0006
Neil MacVicar <n.macvicar9@gmail.com>
2017-11-13 16:05:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ function whisper(string){
 
 
 function logShout(string){
-  console.log(string.toUpperCase)
+  console.log(string.toUpperCase())
 }
 
 
 function logWhisper(string){
-  console.log(string.toLowerCase)
+  console.log(string.toLowerCase())
 }
 
 
@@ -22,10 +22,10 @@ function sayHiToGrandma(string){
   if (string === "I love you, Grandma."){
     return "I love you, too."
   }
-  else if (string === string.toUpperCase){
+  else if (string === string.toUpperCase()){
     return "YES INDEED!"
   }
-  else if (string === string.toLowerCase){
+  else if (string === string.toLowerCase()){
     return "I can\'t hear you!"
   }
 }
